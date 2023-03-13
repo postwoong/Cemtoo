fea1776f32ecd1771b11d9c5cd372ff85f4ecf9e
postwoong <ktw5026@naver.com>
2023-03-13 16:55:07 +0900

    Initial commit

---
+++ src/test/java/com/example/cemtoo/CemtooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cemtoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemtooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
